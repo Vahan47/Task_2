74709cae407a0ee70f2511873fc85b633fc4cb46
Vahan47 <vahan.hakobyan.00@gmail.com>
2020-09-14 19:21:10 +0400

    Task2

---
+++ js/js.js
@@ -0,0 +1,6 @@
+const pi = Math.PI;
+let r = parseFloat(prompt("Enter Radius Value"));
+let s = pi * r * r;
+let p = 2 * pi * r;
+alert("Your Circle Area Is " + s);
+alert("Your Circle Perimeter Is " + p);
